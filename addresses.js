f80a732529bb549f8d00ce7feb0024376fd4bbd1
username1565 <39200328+username1565@users.noreply.github.com>
2022-04-05 11:12:10 +0300

    Update addresses.js

--- addresses.js
+++ addresses.js
@@ -1,1 +1,61 @@
-var addresses = {	'btc'	:	['Bitcoin (BTC)',												'1cvXfGucsRredtDK4UQB8jwYG3Yqm3TC5'				],	'ltc'	:	['LiteCoin (LTC)',												'MQFy7DKEt2wuq5BJx3ch6Cr6RUi7Prshtr'			],	'doge'	:	['Dogecoin (DOGE)',												'DEDTFTVaLaPdzMPo39a2F28UpM8qGkgSTK'			],	'eth'	:	['Ethereum (ETH)',												'0x3D574475714520054D4aAABDf28832894fE35BD3'	],	'erc20'	:	['ERC20 (USDT, USDC, tokens)',									'0x3D574475714520054D4aAABDf28832894fE35BD3'	],	'trx'	:	['Tron (TRX)',													'TGM8zXQsm2Fi7CKnqw1d4cUYPKBmtaftrq'			],	'trc20'	:	['TRC-20 (USDT)',												'TGM8zXQsm2Fi7CKnqw1d4cUYPKBmtaftrq'			],	'rvn'	:	['RavenCoin (RVN)', 											'RNBzvqarGsE4LC8BkAQQP7Z8qcrC1Yz3AM'			]};var selected = 'btc';function initialize(){	var html = ''	html += '<select id="currency" name="select" style="display: inline-block;" onclick="update_address()"> <!--Supplement an id here instead of using "name"-->';	Object.entries(addresses).forEach(([key, value]) => {		html += '<option value="'+key+'"	'+ ( ( key === selected ) ? 'selected' : '' ) + '>'+ value[0] + '</option>' ;	});		html += '</select>';		document.getElementById('select_currency').innerHTML = html;		update_address();}function update_address(){	var currency = document.getElementById('currency');//	console.log(currency.value);	if(currency.value in addresses){		document.getElementById('address').innerHTML = addresses[currency.value][1];	}}/*function copy_to_clipboard(text){	navigator.clipboard.writeText(text);}*/function CopyToClipboard(text) {	console.log(text);	if(navigator.clipboard) {		navigator.clipboard.writeText(text);	}	else{		alert(text);	}  var tooltip = document.getElementById("myTooltip");  tooltip.innerHTML = "Copied!";//"Copied: " + text;}function ResetTooltip() {  var tooltip = document.getElementById("myTooltip");  tooltip.innerHTML = "Copy";}
+var addresses = {
+	'btc'	:	['Bitcoin (BTC)',												'1cvXfGucsRredtDK4UQB8jwYG3Yqm3TC5'				]
+,	'ltc'	:	['LiteCoin (LTC)',												'MQFy7DKEt2wuq5BJx3ch6Cr6RUi7Prshtr'			]
+,	'doge'	:	['Dogecoin (DOGE)',												'DEDTFTVaLaPdzMPo39a2F28UpM8qGkgSTK'			]
+,	'eth'	:	['Ethereum (ETH)',												'0x3D574475714520054D4aAABDf28832894fE35BD3'	]
+,	'erc20'	:	['ERC20 (USDT, USDC, tokens)',									'0x3D574475714520054D4aAABDf28832894fE35BD3'	]
+,	'trx'	:	['Tron (TRX)',													'TGM8zXQsm2Fi7CKnqw1d4cUYPKBmtaftrq'			]
+,	'trc20'	:	['TRC-20 (USDT)',												'TGM8zXQsm2Fi7CKnqw1d4cUYPKBmtaftrq'			]
+,	'rvn'	:	['RavenCoin (RVN)', 											'RNBzvqarGsE4LC8BkAQQP7Z8qcrC1Yz3AM'			]
+};
+
+var selected = 'rvn';
+
+function initialize(){
+
+	var html = ''
+	html += '<select id="currency" name="select" style="display: inline-block;" onclick="update_address()"> <!--Supplement an id here instead of using "name"-->';
+
+	Object.entries(addresses).forEach(([key, value]) => {
+		html += '<option value="'+key+'"	'+ ( ( key === selected ) ? 'selected' : '' ) + '>'+ value[0] + '</option>' ;
+	});
+	
+	html += '</select>';
+	
+	document.getElementById('select_currency').innerHTML = html;
+	
+	update_address();
+}
+
+function update_address(){
+	var currency = document.getElementById('currency');
+//	console.log(currency.value);
+	if(currency.value in addresses){
+		document.getElementById('address').innerHTML = addresses[currency.value][1];
+	}
+}
+
+/*
+function copy_to_clipboard(text){
+	navigator.clipboard.writeText(text);
+}
+*/
+
+function CopyToClipboard(text) {
+	console.log(text);
+
+	if(navigator.clipboard) {
+		navigator.clipboard.writeText(text);
+	}
+	else{
+		alert(text);
+	}
+
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copied!";//"Copied: " + text;
+}
+
+function ResetTooltip() {
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copy";
+}
